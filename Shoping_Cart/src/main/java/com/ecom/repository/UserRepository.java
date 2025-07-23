1ac2799d70b66f2e95abec98579ff7ecf793eb6a
ShwetaSingh1903 <shwetasingh.sde@gmail.com>
2025-07-23 18:34:51 +0530

    First Commit

--- Shoping_Cart/src/main/java/com/ecom/repository/UserRepository.java
+++ Shoping_Cart/src/main/java/com/ecom/repository/UserRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
 
 import com.ecom.model.UserDtls;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserDtls, Integer> {
 
 	public UserDtls findByEmail(String email);
